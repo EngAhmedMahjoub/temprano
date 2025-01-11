8941ae5b8f4de6765782f904da22a36686c93f40
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2025-01-12 02:11:03 +1100

    load .env file token variables

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 from typing import final
 
-BOT_USERNAME: final = ''
+BOT_USERNAME: final = '@ttemprano_bot'
 
 #set your token in .env file
+load_dotenv()
+
 TOKEN = os.getenv('TOKEN')
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
